2332d4d21765bcfa5e61de7f0b694743ee62b38a
ivancho <ivanquinterodevelop@gmail.com>
2020-08-24 11:50:35 -0500

    inicializando prueba

---
+++ app/src/main/java/com/example/pruebatecnica/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pruebatecnica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Hola mundo, primer commit de prueba tecnica
+    }
+}
